8c9da76989c6eda891fbaeb1bf62d373f616ae6b
mimski <maria.bg.1989@gmail.com>
2025-02-09 16:58:34 +0200

    creating PieController

---
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieShop.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class PieController : Controller
+{
+    private readonly IPieRepository pieRepository;
+    private readonly ICategoryRepository categoryRepository;
+
+    public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+    {
+        this.pieRepository = pieRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public IActionResult List()
+    {
+        return View(this.pieRepository.AllPies);
+    }
+}
