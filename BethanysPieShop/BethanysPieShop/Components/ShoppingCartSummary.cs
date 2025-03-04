7af37f8aa14926ea0527f56ef0b31c4987f4d2d0
mimski <maria.bg.1989@gmail.com>
2025-03-04 15:47:03 +0200

    creating shopping cart summary view component to display the total items in the shopping cart

---
+++ BethanysPieShop/BethanysPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieShop.Repositories;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Components;
+
+public class ShoppingCartSummary : ViewComponent
+{
+    private readonly IShoppingCart shoppingCart;
+
+    public ShoppingCartSummary(IShoppingCart shoppingCart)
+    {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var items = this.shoppingCart.GetShoppingCartItems();
+        this.shoppingCart.ShoppingCartItems = items;
+
+        var shoppingCartViewModel = new ShoppingCartViewModel(this.shoppingCart, this.shoppingCart.GetShoppingCartTotal());
+
+        return View(shoppingCartViewModel);
+    }
+}
